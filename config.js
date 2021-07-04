7999fd905d69fb1626d1b7eef895ded9e89eba52
DFHXVJ <86919335+DFHXVJ@users.noreply.github.com>
2021-07-04 21:20:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Babe à!',
+    introDesc: `Tớ có điều muốn nói
+    Cậu trả lời thật lòng nha`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    title: 'Cậu thích tớ phải không nào😙',
+    desc: 'nếu cậu thoát ra mà không nói gì thì làm ny tớ nha ',
+    btnYes: 'Tớ ghét cậu',
+    btnNo: 'Tớ giữ kín bao lâu nay rồi',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Từ giờ không được bỏ bê anh nha',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //link https://www.facebook.com/profile.php?id=100042561212489
 }
